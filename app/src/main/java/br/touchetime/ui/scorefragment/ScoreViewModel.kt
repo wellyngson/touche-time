7530d6d069f0af2d3537f900e6086b6febb5a190
wellyngson <wellyngson.guedes@gmail.com>
2021-12-30 15:16:04 -0300

    Comment removal from the ScoreViewModel.kt file

--- app/src/main/java/br/touchetime/ui/scorefragment/ScoreViewModel.kt
+++ app/src/main/java/br/touchetime/ui/scorefragment/ScoreViewModel.kt
@@ -24,7 +24,6 @@ class ScoreViewModel : ViewModel() {
 
         mutableScoreRed.value = UiState.Success(++scoreRedInitial)
 
-        // Check if scoreRed is big that scoreBlue and change your state
         if (checkVictory(scoreRedInitial, mutableScoreBlue.value.hashCode())) {
             mutableScoreRed.value = UiState.Finish(scoreRedInitial)
         }
@@ -47,7 +46,6 @@ class ScoreViewModel : ViewModel() {
 
         mutableScoreBlue.value = UiState.Success(++scoreBlueInitial)
 
-        // Check if scoreBlue is big that scoreBlue and change your state
         if (checkVictory(scoreBlueInitial, mutableScoreRed.value.hashCode())) {
             mutableScoreBlue.value = UiState.Finish(scoreBlueInitial)
         }
@@ -77,67 +75,4 @@ class ScoreViewModel : ViewModel() {
         data class Finish(val scoreFinal: Int) : UiState()
         data class Error(val score: Int) : UiState()
     }
-
-//    private val mutableScoreRed = MutableLiveData(0)
-//    private val mutableScoreBlue = MutableLiveData(0)
-//    private val mutableFoulRed = MutableLiveData(0)
-//    private val mutableFoulBlue = MutableLiveData(0)
-//
-//    private val mutableTechnicalSuperiority = MutableLiveData(10)
-//
-//    val scoreRed: LiveData<Int> = mutableScoreRed
-//    val scoreBlue: LiveData<Int> = mutableScoreBlue
-//    val foulRed: LiveData<Int> = mutableFoulRed
-//    val foulBlue: LiveData<Int> = mutableFoulBlue
-//
-//    val technicalSuperiority: LiveData<Int> = mutableTechnicalSuperiority
-//
-//    fun addScore(color: String) {
-//        val scoreFinal: Int
-//
-//        if (color == "red") {
-//            scoreFinal = mutableScoreRed.value!! + 1
-//            mutableScoreRed.postValue(scoreFinal)
-//        } else {
-//            scoreFinal = mutableScoreBlue.value!! + 1
-//            mutableScoreBlue.postValue(scoreFinal)
-//        }
-//    }
-//
-//    fun removeScore(color: String) {
-//        val scoreFinal: Int
-//        val scoreInitial: Int
-//
-//        if (color == "red") {
-//            scoreInitial = mutableScoreRed.value!!
-//
-//            if (scoreInitial > 0) {
-//                scoreFinal = scoreInitial - 1
-//                mutableScoreRed.postValue(scoreFinal)
-//            }
-//        } else {
-//            scoreInitial = mutableScoreBlue.value!!
-//
-//            if (scoreInitial > 0) {
-//                scoreFinal = scoreInitial - 1
-//                mutableScoreBlue.postValue(scoreFinal)
-//            }
-//        }
-//    }
-//
-//    fun addFoul(athleteColor: String) {
-//        if (athleteColor == "red") {
-//            mutableFoulRed.postValue(mutableFoulRed.value!! + 1)
-//
-//            if (mutableFoulRed.value!! == 2) {
-//                addScore(athleteColor)
-//            }
-//        } else {
-//            mutableFoulBlue.postValue(mutableFoulBlue.value!! + 1)
-//
-//            if (mutableFoulBlue.value!! == 2) {
-//                addScore(athleteColor)
-//            }
-//        }
-//    }
 }
