d500a7043ac317508d294590feab9127075f7476
wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-01-07 23:12:59 -0300

    Changing a viewModel that will later be deleted

--- app/src/main/java/br/touchetime/ui/editfightfragment/EditFightViewModel.kt
+++ app/src/main/java/br/touchetime/ui/editfightfragment/EditFightViewModel.kt
@@ -3,104 +3,77 @@ package br.touchetime.ui.editfightfragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.touchetime.extension.changeTimer
 
 class EditFightViewModel : ViewModel() {
 
-    private val mutableTechnicalSuperiority = MutableLiveData(1)
-    private val mutableNumberRound = MutableLiveData(1)
-    private val mutableTimeRound = MutableLiveData("03:00")
-    private val mutableTimeInterval = MutableLiveData("00:30")
+    private val mutableTechnicalSuperiorityEditFight = MutableLiveData(1)
+    private val mutableNumberRoundEditFight = MutableLiveData(1)
+    private val mutableTimeRoundEditFight = MutableLiveData("03:00")
+    private val mutableTimeIntervalEditFight = MutableLiveData("00:30")
 
-    val technicalSuperiority: LiveData<Int> = mutableTechnicalSuperiority
-    val numberRound: LiveData<Int> = mutableNumberRound
-    val timeRound: LiveData<String> = mutableTimeRound
-    val timeInterval: LiveData<String> = mutableTimeInterval
+    val technicalSuperiorityEditFight: LiveData<Int> = mutableTechnicalSuperiorityEditFight
+    val numberRoundEditFight: LiveData<Int> = mutableNumberRoundEditFight
+    val timeRoundEditFight: LiveData<String> = mutableTimeRoundEditFight
+    val timeIntervalEditFight: LiveData<String> = mutableTimeIntervalEditFight
 
     fun addTechnicalSuperiority() {
-        var technicalSuperiorityFinal = mutableTechnicalSuperiority.value!!
+        var technicalSuperiorityFinal = mutableTechnicalSuperiorityEditFight.value!!
 
-        mutableTechnicalSuperiority.postValue(++technicalSuperiorityFinal)
+        mutableTechnicalSuperiorityEditFight.postValue(++technicalSuperiorityFinal)
     }
 
     fun removeTechnicalSuperiority() {
-        var technicalSuperiorityFinal = mutableTechnicalSuperiority.value!!
+        var technicalSuperiorityFinal = mutableTechnicalSuperiorityEditFight.value!!
 
         if (technicalSuperiorityFinal > 1) {
-            mutableTechnicalSuperiority.postValue(--technicalSuperiorityFinal)
+            mutableTechnicalSuperiorityEditFight.postValue(--technicalSuperiorityFinal)
         } else {
-            mutableTechnicalSuperiority.postValue(1)
+            mutableTechnicalSuperiorityEditFight.postValue(1)
         }
     }
 
     fun addNumberRounds() {
-        var numberRoundFinal = mutableNumberRound.value!!
+        var numberRoundFinal = mutableNumberRoundEditFight.value!!
 
-        mutableNumberRound.postValue(++numberRoundFinal)
+        mutableNumberRoundEditFight.postValue(++numberRoundFinal)
     }
 
     fun removeNumberRound() {
-        var numberRoundFinal = mutableNumberRound.value!!
+        var numberRoundFinal = mutableNumberRoundEditFight.value!!
 
         if (numberRoundFinal > 1) {
-            mutableNumberRound.postValue(--numberRoundFinal)
+            mutableNumberRoundEditFight.postValue(--numberRoundFinal)
         } else {
-            mutableNumberRound.postValue(1)
+            mutableNumberRoundEditFight.postValue(1)
         }
     }
 
     fun addTimeRound() {
-        val minutes = mutableTimeRound.value!!.substring(0, 2).toInt()
-        val seconds = mutableTimeRound.value!!.substring(3).toInt()
+        val minutes = mutableTimeRoundEditFight.value!!.substring(0, 2).toInt()
+        val seconds = mutableTimeRoundEditFight.value!!.substring(3).toInt()
 
-        mutableTimeRound.postValue(changeTimer(minutes, seconds, 10))
+        mutableTimeRoundEditFight.postValue(changeTimer(minutes, seconds, 10))
     }
 
     fun addTimeInterval() {
-        val minutes = mutableTimeInterval.value!!.substring(0, 2).toInt()
-        val seconds = mutableTimeInterval.value!!.substring(3).toInt()
+        val minutes = mutableTimeIntervalEditFight.value!!.substring(0, 2).toInt()
+        val seconds = mutableTimeIntervalEditFight.value!!.substring(3).toInt()
 
-        mutableTimeInterval.postValue(changeTimer(minutes, seconds, 10))
+        mutableTimeIntervalEditFight.postValue(changeTimer(minutes, seconds, 10))
     }
 
     fun removeTimeRound() {
-        val minutes = mutableTimeRound.value!!.substring(0, 2).toInt()
-        val seconds = mutableTimeRound.value!!.substring(3).toInt()
+        val minutes = mutableTimeRoundEditFight.value!!.substring(0, 2).toInt()
+        val seconds = mutableTimeRoundEditFight.value!!.substring(3).toInt()
 
-        mutableTimeRound.postValue(changeTimer(minutes, seconds, -10))
+        mutableTimeRoundEditFight.postValue(changeTimer(minutes, seconds, -10))
     }
 
     fun removeTimeInterval() {
-        val minutes = mutableTimeInterval.value!!.substring(0, 2).toInt()
-        val seconds = mutableTimeInterval.value!!.substring(3).toInt()
+        val minutes = mutableTimeIntervalEditFight.value!!.substring(0, 2).toInt()
+        val seconds = mutableTimeIntervalEditFight.value!!.substring(3).toInt()
 
-        mutableTimeInterval.postValue(changeTimer(minutes, seconds, -10))
-    }
-
-    private fun changeTimer(minutes: Int, seconds: Int, value: Int): String {
-        return if (minutes == 0 && seconds == 10 && value == -10) {
-            stringFormat(seconds, minutes)
-        } else {
-            var secondsFinal = seconds
-            secondsFinal += value
-
-            when {
-                secondsFinal > 50 -> {
-                    stringFormat(0, minutes + 1)
-                }
-                secondsFinal < 0 -> {
-                    stringFormat(50, minutes - 1)
-                }
-                else -> {
-                    stringFormat(secondsFinal, minutes)
-                }
-            }
-        }
-    }
-
-    private fun stringFormat(seconds: Int, minutes: Int): String {
-        val secondsFinal = String.format("%02d", seconds)
-        val minutesFinal = String.format("%02d", minutes)
-
-        return "$minutesFinal:$secondsFinal"
+        mutableTimeIntervalEditFight.postValue(changeTimer(minutes, seconds, -10))
     }
 }
