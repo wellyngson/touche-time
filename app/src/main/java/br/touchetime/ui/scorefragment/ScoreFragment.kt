521597fdda7e0d6fc213eb6afc03e70efaeb7864
wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-01-07 23:13:50 -0300

    Remove import.

--- app/src/main/java/br/touchetime/ui/scorefragment/ScoreFragment.kt
+++ app/src/main/java/br/touchetime/ui/scorefragment/ScoreFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
@@ -13,7 +12,6 @@ import androidx.lifecycle.lifecycleScope
 import br.touchetime.MainActivity
 import br.touchetime.databinding.FragmentScoreBinding
 import br.touchetime.ui.chronometerfragment.ChronometerFragment
-import br.touchetime.ui.editfightfragment.EditFightFragment
 import br.touchetime.ui.homefragment.HomeFragment
 import kotlinx.coroutines.flow.collect
 
