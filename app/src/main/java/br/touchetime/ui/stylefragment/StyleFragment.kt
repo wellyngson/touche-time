7b272f1b762559210ba41e756c046472a6e4a0b8
wellyngson <59460244+wellyngson@users.noreply.github.com>
2022-01-17 23:43:26 -0300

    Create StyleFragment and package

--- app/src/main/java/br/touchetime/ui/stylefragment/StyleFragment.kt
+++ app/src/main/java/br/touchetime/ui/stylefragment/StyleFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
+import br.touchetime.data.model.Fight
 import br.touchetime.databinding.FragmentStyleBinding
 import br.touchetime.ui.bottomcontrol.BottomSheetDialogTransparentBackgroundFragment
 
@@ -30,25 +32,57 @@ class StyleFragment : BottomSheetDialogTransparentBackgroundFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        readArgs()
         setupAdapter()
+        setupFinishBottomSheet()
+    }
+
+    private fun readArgs() {
+        (arguments?.getParcelable(ARG_FIGHT) as? Fight)?.let { fight ->
+            viewModel.setFight(fight)
+        }
     }
 
     private fun setupAdapter() {
         viewBinding.style.adapter = StyleAdapter(
             viewModel.getListStyle()
-        ) {
-            onStyleSelected(it)
+        ) { style ->
+            onStyleSelected(getString(style))
         }
     }
 
-    private fun onStyleSelected(style: Int) {
+    private fun setupFinishBottomSheet() {
+        viewBinding.finish.setOnClickListener {
+            onStyleSelectedResult()
+        }
+    }
+
+    private fun onStyleSelected(style: String) {
+        viewModel.setStyleSelected(style)
+    }
+
+    private fun onStyleSelectedResult() {
+        parentFragmentManager.setFragmentResult(
+            STYLE_SELECTED,
+            bundleOf(
+                FIGHT to viewModel.getFight()
+            )
+        )
     }
 
     companion object {
         const val TAG = "br.touchetime.ui.stylefragment"
+        const val FIGHT = "FIGHT"
+        const val STYLE_SELECTED = "STYLE_SELECTED"
+        private const val ARG_FIGHT = "ARG_FIGHT"
 
-        private fun newInstance() = StyleFragment()
+        private fun newInstance(fight: Fight?) = StyleFragment().apply {
+            arguments = bundleOf(
+                ARG_FIGHT to fight
+            )
+        }
 
-        fun show(fragmentManager: FragmentManager) = newInstance().show(fragmentManager, TAG)
+        fun show(fragmentManager: FragmentManager, fight: Fight?) =
+            newInstance(fight).show(fragmentManager, TAG)
     }
 }
--- app/src/main/java/br/touchetime/ui/stylefragment/StyleViewModel.kt
+++ app/src/main/java/br/touchetime/ui/stylefragment/StyleViewModel.kt
@@ -1,9 +1,22 @@
 package br.touchetime.ui.stylefragment
 
 import androidx.lifecycle.ViewModel
+import br.touchetime.data.model.Fight
 import br.touchetime.data.model.StyleFight
 
 class StyleViewModel : ViewModel() {
+
+    private var fight: Fight? = null
+
+    fun setFight(fight: Fight) {
+        this.fight = fight
+    }
+
+    fun getFight(): Fight? = fight
+
+    fun setStyleSelected(style: String) {
+        this.fight?.style = style
+    }
 
     fun getListStyle(): List<Int> = StyleFight.getListStyle()
 }
